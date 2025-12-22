fd46a0397bc1931bcc182f758f617525b8a1eaad
James Heaton <heaton1138@gmail.com>
2025-12-22 12:51:31 -0600

    Login & Register Forms Created / Auth Flow Working

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,72 @@
-import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from '../context/AuthContext';
 
 export const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+
+    if (!username || !password) {
+      setError('Username and password required');
+      return;
+    };
+
+    setLoading(true);
+
+    try {
+      await login(username, password);
+      navigate('/characters');
+    } catch (err) {
+      setError(err.message || 'Login failed. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
-      <h1>Login Page</h1>
-      <p>Login Form Will Go Here</p>
-      <Link to="/characters">Sign In</Link>
-      <Link to="/register">Sign Up</Link>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            disabled={loading}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={loading}
+          />
+        </div>
+
+        {error && <div style={{ color: 'red' }}>{error}</div>}
+        
+        <button type="submit" disabled={loading}>
+          {loading ? 'Signing in...' : 'Sign In'}
+        </button>
+
+        <p>
+        First Adventure? <Link to="/register">Sign Up</Link>
+        </p>
+      </form>
     </div>
-  );
+  )
 };
